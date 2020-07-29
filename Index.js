0045ebc955501c521b59720d27f0129798c90617
lafry5 <lafry5@yahoo.com>
2020-07-29 11:09:00 -0700

    started Employee tests

--- Index.js
+++ Index.js
@@ -1,26 +1,38 @@
 const fs = require('fs');
 const inquirer = require("inquirer")
-// let Manager = require("Manager.js")
-// let Intern = require("Intern.js") // Made available the intern class
+let Manager = require("./lib/Manager.js")
+let Intern = require("./lib/Intern.js") 
+let Employee = require("./lib/Employee.js") 
 answers = [];
 data = [];
 
+// const output_directory = path.resolve(directoryname, "output")
+// const outputPath = path.join(output_directory, "Team.html");
 
 const generatePage = require('./src/generateTeam.js'); //says generatePage is not a function 
 
 
 
 // function to write Team file
-function writeToFile(fileName, data) {
+
+// function writeToFile(fileName, data) {
   
-    fs.writeFile("Team.html", generatePage(data), (err) => {
-            if (err) 
-                console.log(err);
-          else {
-            console.log('Team.html complete! Check it out!');
-          }
-        });
+//     fs.writeFile("Team.html", generatePage(data), (err) => {
+//             if (err) 
+//                 console.log(err);
+//           else {
+//             console.log('Team.html complete! Check it out!');
+//           }
+//         });
+// }
+
+function compileTeam() {
+  if(!fs.existsSync(output_directory)) {
+    fs.mkdirSync(output_directory)
+  }
+  fs.writeFileSync(outputPath, generatePage(teamMembers), "utf-8") // received assistance with this; need to understand this better
 }
+
 
 //If role = intern then create new Instance
 // let Bob (instance name) = new Intern (input from inquirer(1), id input, email input, etc)
@@ -40,7 +52,7 @@ inquirer
       },
       {
         type: 'input',
-        name: 'identification',
+        name: 'id',
         message: 'Enter your ID'
       },
       {
@@ -58,6 +70,11 @@ inquirer
         type: 'input',
         name: 'officenumber',
         message: 'What is your office phone number [use (xxx)xxx-xxxx format]?' // Manager only
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?' // Engineer only
       },
       {
         type: 'input',
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,24 @@
-// jest.mock('../lib/Potion');
+const Employee = require("../lib/Employee.js")
 
-// console.log(new Potion());
+test ("Can instantiate Employee instance", function() {
+    const E = new Employee();
+    expect(typeof(E)).toBe("object");
+})
+
+test ("Can set name through constructor arguments", function() {
+    const name = "Laura";
+    const E = new Employee(name);
+    expect(E.name).toBe(name);
+})
+
+/// Do same for ID, email, role
+
+test ("Can get name via getName function", function() {
+    const testValue = "Laura";
+    const E = new Employee(testValue);
+    expect(E.getName()).toBe(testValue);
+})
+
+/// Do same for ID, email, role
 
 
